87456ac11677650411f26e510ddc52c931aa15f4
bentoherbert <100323996+bentoherbert@users.noreply.github.com>
2022-09-03 01:23:00 -0300

    Commit
    Atualização no html, css e js.

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-var formulario = document.querySelector('form')
+var formulario = document.querySelector("form");
 
-formulario.addEventListener('submit', function(e){
+formulario.addEventListener("submit", function (e) {
   // Bloqueia o refresh da página
-  e.preventDefault()
+  e.preventDefault();
   // Url da pesquisa
   let urlForm = "https://pokeapi.co/api/v2/pokemon/";
   // Valor do input Name
-  let nome = document.getElementById("name")
+  let nome = document.getElementById("name");
+  // Concatena a url com o inputname
+  urlForm = urlForm + this.name.value;
+  // Transforma os valores em minúsculos
+  urlForm = urlForm.toLocaleLowerCase();
 
-  urlForm = urlForm + this.name.value
+  // ID content
+  let resposta = document.getElementById("content");
 
-  alert(urlForm)
+  // ID ImgPokemon
+  let imagem = document.getElementById("imgPokemon");
+
+  // Resposta em HTML
+  let html = "";
+
+  fetch(urlForm)
+    .then((resposta) => resposta.json())
+    .then(function (data) {
+      console.log(data);
+      html = "Nome: " + maiuscula(data.name) + "<br>";
+      html = html + "Type: " + maiuscula(data.types[0].type.name);
+      resposta.innerHTML = html;
+
+      imagem.innerHTML =
+        "<img src='" +
+        data.sprites.front_default +
+        "'><img src='" +
+        data.sprites.back_default +
+        "'>";
+    })
+    .catch(function (err) {
+      if (err == "SyntaxError: Unexpected token N in JSON at position 0") {
+        html = "Pokémon não encontrado! 😣";
+      } else {
+        html = "Erro:" + err;
+      }
+      resposta.innerHTML = html;
+    });
 });
+
+function maiuscula(val) {
+  return val[0].toUpperCase() + val.substr(1);
+}
